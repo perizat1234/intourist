ca2ef84fd5d2dac5e6e483273bb3a08b73c16649
perizat1234 <127853893+perizat1234@users.noreply.github.com>
2023-03-19 18:12:19 +0600

    add core models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField( to=User, on_delete= models.CASCADE,
+    related_name='profile'
+    ),
+    region = models.CharField(max_length=1,
+     choices=(
+     ('B', 'Bishkek'),
+     ('O', 'Osh'),
+     ('N', 'Naryn'),
+     ('A', 'Batken'),
+     ('I', 'Issyk-kul'),
+     ('D', 'Djalal-abad'),
+     ('T', 'Talas'),
+     ))
+    foto = models.ImageField(
+        upload_to='profile_photo',
+        null=True, blank=True)
+    
+    def __str__(self): 
+      return self.user.username     
+
--- points/models.py
+++ points/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Point (models.Model):
+    user = models.ForeignKey(
+       to = User, on_delete = models.SET_NULL,
+       null=True, blank=True
+    )
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     views_count = models.IntegerField(default=0)
